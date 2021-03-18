847e632c5aee31c8ec4d84e80af01514b564c5f4
deepak-padampur <deepaksuna4290@gmail.com>
2021-03-18 12:07:08 +0530

    actual data added

--- src/components/videoThumbnail.js/index.js
+++ src/components/videoThumbnail.js/index.js
@@ -5,10 +5,16 @@ import './style.css'
 function VideoThumbnail({ video }) {
   const [embeddedHTML, setEmbeddedHTML] = useState('')
   const [thumbnailSrc, setThumbnailSrc] = useState('')
+  const [title, setTitle] = useState('')
+  const [channelTitle, setChannelTitle] = useState('')
+  const [viewCount, setViewCount] = useState('')
 
   useEffect(() => {
     if (video) {
       setThumbnailSrc()
+      setTitle(video.snippet.title)
+      setChannelTitle(video.snippet.channelTitle)
+      setViewCount(video.statistics.viewCount)
     }
 
   }, [video])
@@ -18,17 +24,17 @@ function VideoThumbnail({ video }) {
 
     <article className="video-container">
       <a href="#" className="thumbnail" data-duration="12:24">
-        <img className="thumbnail-image" src={thumbnailSrc} />
+        <img className="thumbnail-image" src="http://unsplash.it/36/36?gravity=center" />
       </a>
       <div className="video-bottom-section">
         <a href="#">
           <img className="channel-icon" src="http://unsplash.it/36/36?gravity=center" />
         </a>
         <div className="video-details">
-          <a href="#" className="video-title">Video Title</a>
-          <a href="#" className="video-channel-name">Channel Name</a>
+          <a href="#" className="video-title">{title}</a>
+          <a href="#" className="video-channel-name">{channelTitle}</a>
           <div className="video-metadata">
-            <span>12K views</span>
+            <span>{viewCount} views</span>
             •
               <span>1 week ago</span>
           </div>
